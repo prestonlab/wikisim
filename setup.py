a6e54d213c7bffc45316b84bd86a04590b683e78
Neal Morton <mortonne@gmail.com>
2020-11-03 18:40:59 -0600

    build: add build config files

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+import glob
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+scripts = glob.glob('bin/*.sh') + glob.glob('bin/*.py')
+
+setuptools.setup(
+    name='wikisim',
+    version='1.0.0',
+    description='Neural representation of semantic similarity for famous people and places.',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author='Neal Morton',
+    author_email='mortonne@gmail.com',
+    license='GPLv3',
+    url='http://github.com/prestonlab/wikisim',
+    packages=setuptools.find_packages(),
+    package_dir={'wikisim': 'src'},
+    package_data={'wikisim': ['resources/subjects.json']},
+    scripts=scripts,
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
